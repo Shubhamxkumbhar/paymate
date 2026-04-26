acdebc6e0dcb969ef67d3f345dd226f49cd15eb4
x22119345 <x22119345@student.ncirl.ie>
2026-04-26 21:25:56 +0100

    feat: initial project setup from Spring Initializr

---
+++ src/main/java/com/paymate/paymate/PaymateApplication.java
@@ -0,0 +1,13 @@
+package com.paymate.paymate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paymate/paymate/PaymateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paymate.paymate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
